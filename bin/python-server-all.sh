0d7da08f126c7617427d74339aa699f1ef70a0d6
Sam Johnston <samj@samj.net>
2020-05-25 21:22:42 +0800

    Add python-aws-apigateway-dynamodb
    Added new AWS API Gateway via Lambda to DynamoDB generator (python-aws-apigateway-dynamodb) based on python-flask

--- bin/python-server-all.sh
+++ bin/python-server-all.sh
@@ -2,6 +2,7 @@
 
 ./bin/python-server-aiohttp-petstore.sh
 ./bin/python-server-aiohttp-srclayout-petstore.sh
+./bin/python-server-aws-apigateway-dynamodb-petstore.sh
 ./bin/python-server-flask-petstore.sh
 ./bin/python-server-flask-petstore-python2.sh
 ./bin/python-server-blueplanet-petstore.sh
